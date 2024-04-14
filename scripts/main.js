6560d5ab220148f023aae6892f2c220a9bbf53d8
ianbastos <ianbastos@gmail.com>
2024-04-14 20:01:46 -0300

    mais um

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,19 @@ if (iceCream === "chocolate") {
 } else {
   alert("Aaaah, mas chocolate é o meu favorito…");
 }
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
+  document.querySelector("h1").addEventListener("click", () => {
+    alert("Ai! Pare de me cutucar!");
+  });
